c1628af0266db29003cd813ee66a7a1a49d32795
Blmoeller <43049946+Blmoeller@users.noreply.github.com>
2022-10-20 22:10:13 -0600

    initial commit for testing files and github actions

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,10 +1,15 @@
-# remember to import your libraries!
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append('..')
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+    return None
+
 
 def get_file_dimensions(file_name):
-	return (0,0)
+    return (0, 0)
+
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+    return None
