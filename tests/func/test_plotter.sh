ef027f33fb639a9221dc8ec62474c8bfafa974dd
Blmoeller <43049946+Blmoeller@users.noreply.github.com>
2022-10-23 15:48:20 -0600

    Functional tests for plotter (most of these should fail)

--- tests/func/test_plotter.sh
+++ tests/func/test_plotter.sh
@@ -3,4 +3,20 @@ set -o pipefail
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
-echo 'maybe'
+run test_in_stdout python3 src/plotter.py
+assert_in_stdout "This created 3 plots: iris_boxplot.png,
+petal_length_v_width_scatter.png, and multi_panel_figure.png."
+
+run test_stdout python3 src/plotter.py
+assert_in_stdout "Finished making plots"
+
+run test_stdout python3 src/plotter.py
+assert_stdout 
+
+echo 'Looking for image files'
+[ -s src/iris_boxplot.png ] && echo "Found" || echo "Not found"
+[ -s src/petal_length_v_width_scatter.png ] && echo "Found" || echo "Not found"
+[ -s src/multi_panel_figure.png ] && echo "Found" || echo "Not found"
+
+echo 'Looking for iris data file'
+[ -s iris.data ] && echo "Found" || echo "Not found"
