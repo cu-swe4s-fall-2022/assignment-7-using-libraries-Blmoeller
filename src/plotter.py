1368de592967171b049f1044b651e49b1d5aceeb
Blmoeller <43049946+Blmoeller@users.noreply.github.com>
2022-10-23 11:38:44 -0600

    Initial commit

--- src/plotter.py
+++ src/plotter.py
@@ -1,1 +1,10 @@
-# Your code to create majestic plots goes in here!
+"""
+"""
+import sys
+import numpy as np
+sys.path.append('src')
+import data_processor as dp  # nopep8
+
+
+if __name__ == '__main__':
+    main()
